0fe034f9d6af2d126375e08ba77f6b3383fbe977
galanggg <tugasgalang@gmail.com>
2021-12-26 22:01:25 +0800

    feat: add my first function

---
+++ netlify/functions/hello.js
@@ -0,0 +1,10 @@
+exports.handler = async (event, context) => {
+  console.log("Hi from the Angular Serverless App");
+  console.log(`\nHere is the event info: ${JSON.stringify(event)}`);
+  console.log(`\nHere is the context info: ${JSON.stringify(context)}`);
+
+  return {
+    statusCode: 200,
+    body: "This is working yesss!",
+  };
+};
