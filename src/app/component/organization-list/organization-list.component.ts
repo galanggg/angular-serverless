b855b27a090bdb52b163411c4fb4232a5bef5afd
galanggg <tugasgalang@gmail.com>
2022-01-02 12:56:05 +0800

    feat: add payment using stripe

--- src/app/component/organization-list/organization-list.component.ts
+++ src/app/component/organization-list/organization-list.component.ts
@@ -1,5 +1,8 @@
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { loadStripe } from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-organization-list',
@@ -9,7 +12,28 @@ import { Component, OnInit } from '@angular/core';
 export class OrganizationListComponent implements OnInit {
   @Input() organization: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {}
+
+  async triggerCreateCheckout(eventOrganization: any) {
+    const createCheckoutResponse = await this.http.post(
+      '/.netlify/functions/createCheckout',
+      eventOrganization,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const lastResponse = await lastValueFrom(createCheckoutResponse);
+    this.openStripe(lastResponse);
+  }
+
+  openStripe = async (stripeParams: any) => {
+    const stripe = await loadStripe(stripeParams.publishableKey);
+    const { error } = await stripe!.redirectToCheckout({
+      sessionId: stripeParams.sessionId,
+    });
+  };
 }
